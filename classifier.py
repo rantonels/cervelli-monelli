1831ffc930a909a8ea023b677e97f12d43ce9f6d
Riccardo Antonelli <riccardo.antonelli@hotmail.it>
2016-12-15 17:10:01 +0100

    huh

--- classifier.py
+++ classifier.py
@@ -12,13 +12,21 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.naive_bayes import GaussianNB
 from sklearn.tree import DecisionTreeClassifier
 
+
+from sklearn.ensemble import ExtraTreesClassifier
+
+from sklearn.model_selection import train_test_split
+from sklearn.feature_selection import SelectFromModel
+
 labels_names = [ "gender", "age", "health" ]
 
 def load_pickle_dataset(fname):
     
-    db = pickle.load(open(fname,'r'))
+    db = np.load(fname)
 
-    out_array = np.zeros((len(db),db[1].size))
+    return db
+
+    out_array = np.zeros((db.shape[0],db.shape[1]))
 
     for i in range(len(db)):
         out_array[i,:] = db[i+1].reshape(-1)
@@ -41,55 +49,111 @@ def train(): # addestra i classificatori e stampa info
             index+=1
 
 
-    samples_array = load_pickle_dataset("features/train_dataset")
+    samples_array = load_pickle_dataset("features/train_dataset.npy")
 
 
-    total_number = samples_array.shape[0]
-    train_number = 230 # number of entries from train_dataset used for training
-    test_number = total_number - train_number
+
+
+
+   # total_number = samples_array.shape[0]
+   # train_number = 230 # number of entries from train_dataset used for training
+   # test_number = total_number - train_number
 
 
     hamming_loss = {}
     
     classifiers = {}
 
-    for label in range(3):
-        
-        targets_array = np.zeros((len(targets)))
-        for i in range(total_number):
-            targets_array[i] = targets[i+1][label]
-
+    for label in range(3): 
         print()
         print("* Classifico labella %s"%labels_names[label])
 
 
-        if label in [1,2]: # non gender, questo se la cava egregiamente
-            classifier = GaussianProcessClassifier(1.0*RBF(1.0), warm_start=True,n_jobs = 9)
+        # Create targets array
+        targets_array = np.zeros((len(targets)))
+        for i in range(len(targets)):
+            targets_array[i] = targets[i+1][label]
 
+
+        # feature selection
+
+
+        if True:
+            print("feature selection...")
+            clf = ExtraTreesClassifier()
+
+            clf = clf.fit(samples_array, targets_array)
+
+            model = SelectFromModel(clf, prefit=True)
+            
+
+            samples_extracted = model.transform(samples_array)
+
+            print("Feature ridotte %d -> %d"% (samples_array.shape[1], samples_extracted.shape[1]))
+        else:
+            samples_extracted = samples_array
+
+
+
+        print("split..")
+
+        # split
+
+        samples_train, samples_test, targets_train, targets_test = train_test_split( 
+                samples_extracted, 
+                targets_array, 
+                test_size=0.2, 
+                random_state=np.random.randint(0,1000)
+                )
+
+
+  
+        print("init..")
+
+        # init classifier
+
+        if label == 1: # eta'
+            #classifier = GaussianProcessClassifier(1.0*RBF(1.0), warm_start=True,n_jobs = 9)
+            #classifier = svm.SVC(gamma=1.0, C = 1.0, class_weight = "balanced"
+            classifier = svm.SVC(kernel="linear")
+            #classifier = GaussianProcessClassifier(2.5*RBF(2.5), warm_start=True,n_jobs = 9,max_iter_predict=1000) # meh, non male for age
+            classifier = DecisionTreeClassifier(max_depth=50, criterion="entropy") # non ci siamo
+        elif label == 2: # salute
+            classifier = svm.SVC(kernel="linear",max_iter=1000000)
+            #classifier = svm.SVC(gamma=1.0, C = 1.0, class_weight = "balanced")
+            #classifier = GaussianProcessClassifier(1.0*RBF(1.0), warm_start=True,n_jobs = 9)
         else: # gender
-
             # questi sono i migliori fin'ora
             
-            #classifier = GaussianProcessClassifier(2.5*RBF(2.5), warm_start=True,n_jobs = 9,max_iter_predict=1000)
-            classifier = DecisionTreeClassifier(max_depth=2, criterion="entropy")
+            #classifier = svm.SVC(gamma='auto', C = 1.0, class_weight = "balanced")
+            #classifier = GaussianProcessClassifier(1.0*RBF(1.0), warm_start=True,n_jobs = 9,max_iter_predict=1000)
+            classifier = svm.SVC(kernel="linear",max_iter=1000000)
+            #classifier = DecisionTreeClassifier(max_depth=10, criterion="entropy")
 
             # questi altri signori, schifo
 
-            #classifier = svm.SVC(gamma=2.0, C = 2.0, class_weight = "balanced")
-            #classifier = svm.SVC(kernel="linear", C=0.025)
+            #classifier = svm.SVC(gamma='auto', C = 100, class_weight = "balanced")
             #classifier = GaussianProcessClassifier(4.0*ConstantKernel(), warm_start=True,n_jobs = 9,max_iter_predict=1000)
-            #classifier = MLPClassifier(alpha=100,activation="tanh",tol=1e-6)
+            #classifier = MLPClassifier(alpha=2.0)
             #classifier = KNeighborsClassifier(10)
-            #classifier = AdaBoostClassifier(n_estimators = 5000)
+        
+            #classifier = AdaBoostClassifier(n_estimators = 500) # good for age
+        #classifier = MLPClassifier(alpha=3.0,activation="tanh",tol=1e-6) # a volte good for age
 
-        classifier.fit(samples_array[:train_number],targets_array[:train_number])
 
-        expected_output = targets_array[train_number:].astype(int)
-        predicted_output = classifier.predict(samples_array[train_number:]).astype(int)
+        #classifier = svm.SVC(kernel="linear", C= 1.0) # nah
+        
+
+        print("fit..")
+
+        classifier.fit(samples_train,targets_train)
+
+        expected_output = targets_test.astype(int)
+        predicted_output = classifier.predict(samples_test).astype(int)
     
 
-        print("\tOutput atteso:  \t",blist2str(expected_output))
-        print("\tOutput predetto:\t",blist2str(predicted_output))
+        print("\tOutput atteso:  \t",blist2str(expected_output[0:50]),"...")
+        print("\tOutput predetto:\t",blist2str(predicted_output[0:50]),"...")
 
         print("\t",metrics.classification_report(expected_output,predicted_output))
 
@@ -116,6 +180,7 @@ def train(): # addestra i classificatori e stampa info
 good_classifiers = train()
 
 
+exit()
 
 test_array = load_pickle_dataset("features/test_dataset")
 
--- flattener.py
+++ flattener.py
@@ -6,28 +6,120 @@ import os
 import scipy.signal
 from tqdm import tqdm
 import pickle
+import csv
 
-t_side = 2
+t_side = 10
+n_split = 10
 
 target_shape = np.array([t_side,t_side,t_side,1])
 
+fisher = np.load("data/fisher.npz")["arr_0"]
+quantile_threshold = 0.95
+fisher_threshold = scipy.stats.mstats.mquantiles(
+    fisher[fisher > 0].ravel(),
+    prob=quantile_threshold)[0]
+
+fisher_mask = fisher > fisher_threshold
+
+
 def open_and_flatten(filename):
+
     brain_scan = nib.load(filename)
     brain_array = brain_scan.get_data().mean(-1)
+#
+#
+    mean = np.mean(brain_array)
+    median = np.median(brain_array)
+    variance = np.var(brain_array)
+#    grey_matter = np.sum(np.greater(brain_array,mean*0.5))/float(np.size(brain_array))
+#
+#
+    brain_trunc = brain_array# brain_array[60:150,40:150,30:150]
+# 
+
+    csf = np.sum(np.logical_and(brain_array > 0, brain_array < 300))
+    gm = np.sum(np.logical_and(brain_array > 650, brain_array < 850))
+    wm = np.sum(np.logical_and(brain_array > 1250, brain_array < 1450))
+
+    #print csf,gm,wm
+
+    #FISHER
+    
+    fisher_feature = np.einsum("ijk,ijk",brain_trunc, fisher_mask)
+    fisher_features = brain_trunc[ fisher_mask ]
+
+    fourier_brain = np.fft.fftn(brain_trunc)
 
 
-    fourier_brain = np.fft.fftn(brain_array)
-
-
-    fourier_slice = fourier_brain[0:t_side,0:t_side,0:t_side]
+    fourier_slice = fourier_brain[0:3,0:3,0:3]
 
     re = np.real(fourier_slice)
     re = re.reshape((re.size,))
     im = np.imag(fourier_slice)[1:]
     im = im.reshape((im.size,))
 
-    out = np.concatenate((re,im)) * 1e-8
-    return out
+    out_fourier = np.concatenate((re,im)) * 1e-8
+#
+# 
+#    # histograms
+#
+#    H = 8
+#
+##    chunks_shape = (np.array(brain_trunc.shape) / np.array([t_side,t_side,t_side]) ).astype(int)
+#
+#
+#    def spl(arr,ax):
+#        return np.array(np.split(arr,n_split,axis=ax))
+#
+#    chunks = spl(spl(spl(brain_trunc,-1),-2),-3)
+#
+#    histograms = np.zeros( (chunks.shape[0:3] )+( H,)  )
+#    histograms += 3
+#
+#    #for x in range(chunks_shape[0]):
+#    #    for y in range(chunks_shape[1]):
+#    #        for z in range(chunks_shape[2]):
+#    #            chunk = brain_trunc[    x*t_side : (x+1)*t_side ,
+#    #                                    y*t_side : (y+1)*t_side,
+#    #                                    z*t_side : (z+1)*t_side
+#    #                                    ]
+#    for x in range(n_split):
+#     for y in range(n_split):
+#      for z in range(n_split):
+#        chunk = chunks[z,y,z,:,:,:]
+#        chunk = chunk.reshape((chunk.size,))
+#        histo, bin_edges =  np.histogram(chunk, H, range = (np.min(brain_trunc),np.max(brain_trunc) ) )
+#
+#        histograms[x,y,z,:] = histo
+#
+#    histograms = histograms.reshape((histograms.size,))
+#
+#
+
+
+#    down_arr = scipy.signal.decimate(brain_trunc, brain_trunc.shape[0]//t_side, axis = 0)
+#    down_arr = scipy.signal.decimate(down_arr, down_arr.shape[1]//t_side, axis = 1)
+#    down_arr = scipy.signal.decimate(down_arr, down_arr.shape[2]//t_side, axis = 2)
+#
+#    down_arr = down_arr.reshape((down_arr.size,))
+
+#
+#    import matplotlib.pyplot as plt
+#
+#    for i in range(t_side):
+#        plt.imshow(down_arr[:,:,i])
+#        plt.show()
+
+
+    return  np.concatenate((
+        np.array([mean,median,variance,csf,gm,wm,fisher_feature]),
+        np.array([]),
+        fisher_features,
+#        out_fourier
+        ))
+#        histograms
+        
+        
 
 
     return np.array([mean])
@@ -62,13 +154,35 @@ d = os.path.dirname(out_path)
 if not os.path.exists(d):
     os.makedirs(d)
 
-train_dataset = {}
+train_dataset = []
+
+features_abis = {}
+with open("features/features_abis.csv",'rb') as csvfile:
+    reader = csv.reader(csvfile,delimiter = ",")
+    first_row = True
+    for row in reader:
+        if first_row:
+            first_row = False
+            continue
+        fid,data = int(row[0]), row[4:]
+        features_abis[fid] = np.array(data)
+
+
 
 for index in tqdm(range(1,entries_total+1)):
-    flattened = open_and_flatten("data/set_train/train_%d.nii"%index)
-    train_dataset[index] = flattened
+    flattened = open_and_flatten("data/cropped/train_%d_cropped.nii.gz"%index)
+    flattened_abis = np.array(features_abis[index])
+    flattened = np.concatenate((flattened,flattened_abis))
+    train_dataset.append(flattened)
 
-pickle.dump(train_dataset, open(os.path.join(out_path,"train_dataset"),'w'))
+print("Converting to np array...")
+
+train_dataset = np.array(train_dataset)
+
+print("Saving...")
+np.save(os.path.join(out_path,"train_dataset"),train_dataset)
+
+exit(1)
 
 test_dataset = {}
 
